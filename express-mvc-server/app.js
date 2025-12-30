a6659899226b62433d4bc0e4d2b167ac2926131e
Aman-149 <amanuelfikadu23971@gmail.com>
2025-12-30 21:34:14 +0300

    Create app.js

---
+++ express-mvc-server/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const mainRoutes = require("./routes/mainRoutes");
+
+app.use(express.json());
+app.use("/api", mainRoutes);
+
+// handle unknown routes
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
